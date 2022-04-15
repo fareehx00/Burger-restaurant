dd6535823538c921c9b89aeed2e17fd8283e9b7c
fareehx00 <101110785+fareehx00@users.noreply.github.com>
2022-04-16 02:30:14 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+// navbar
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 400){
+        navbar.classList.add('fixed');
+    } else{
+        navbar.classList.remove('fixed');
+    }
+})
+ // fixed btn
+const fixedBtn = document.querySelector('.fixed-btn');
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 600){
+        fixedBtn.classList.add('show');
+    } else{
+        fixedBtn.classList.remove('show');
+    }
+})
